92c36cf61fbaa815d23b82ac32f65c1ddb12da7a
nitishpratap18 <nitish@itinker.io>
2023-02-05 23:10:31 +0530

    made button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {NormalButton, RedBackgroundButton} from "./components/buttons/button";
+import {NormalButton, RedBackgroundButton, RentButton} from "./components/buttons/button";
 
 function App() {
     return (
         <div className="App">
-
+            <RedBackgroundButton title={"red"}/>
+            <NormalButton title={"Normal"} height={40} width={120}/>
+            <RentButton/>
         </div>
     );
 }
--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -1,17 +1,45 @@
 import {Button} from "@mui/material";
+import './buttonStyles.css'
+import React, {useState} from "react";
 interface buttonTitle {
     title: string;
+
+}
+interface normalButtonProps {
+    title : String,
+    width : number,
+    height : number,
 }
 export const RedBackgroundButton = (props : buttonTitle) => {
 
     return <div>
-        <Button variant="contained" >{props.title}</Button>
+        <Button variant="contained" style={{ width: 170, height: 40, backgroundColor: '#D3302F',border : "none",color : 'white'}}>{props.title}</Button>
     </div>
 }
 
-export const NormalButton = (props : buttonTitle)=>{
-    return <div>
-        <Button variant="outlined">{props.title}</Button>
-
+export const NormalButton = (props : normalButtonProps)=>{
+    const red : String = '#D3302F'
+    const white : String = '#FFFFFF'
+const [textColor,setTextColor] = useState(red)
+    const handleMouseEnter = () => {
+        setTextColor(white);
+    };
+    const handleMouseLeave = () => {
+        setTextColor(red);
+    };
+    const buttonSX = {
+        "&:hover": {
+            backgroundColor : '#D3302F',
+            color : '#FFFFFF',
+        },
+    };
+    return <div >
+        <Button onMouseEnter={()=>{handleMouseEnter()}} onMouseLeave={()=>{handleMouseLeave()}} sx={buttonSX} variant="outlined" style={{width : props.width, height : props.height, borderColor : '#D3302F', borderWidth : 2, color :  `${textColor}`}} className="normal-button" >{props.title}</Button>
     </div>
 }
+
+export const RentButton = ()=>{
+    return <NormalButton  width={60} height={35} title={"Rent"}  />
+}
+
+
